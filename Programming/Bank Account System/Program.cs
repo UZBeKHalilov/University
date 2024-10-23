e0d65d372bc1cb5a04c50cce6833d19718e4ba5c
Abdulloh Halilov <109743592+UZBeKHalilov@users.noreply.github.com>
2024-10-23 18:45:51 +0500

    Bank Account System
    Completed!

--- Programming/Bank Account System/Program.cs
+++ Programming/Bank Account System/Program.cs
@@ -29,9 +29,100 @@ namespace Bank_Account_System
 {
     internal class Program
     {
+        public static double[] money = { 130.9, 50.6, 13.56 };
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Bank!");
 
+            BankAccount();
         }
+
+        static void BankAccount()
+        {
+            Console.WriteLine($"You have ");
+
+            for (int i = 0; i < money.Length; i++)
+            {
+                Console.WriteLine($"{i+1} - card : {money[i]}$");
+            }
+
+            Console.WriteLine("Choose functions:" +
+                "\n[1] Addmoney" +
+                "\n[2] Subtract money" +
+                "\n[3] Trasfer money");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddMoney();
+
+                    break;
+                case "2":
+                    SubtractMoney();
+                   
+                    break;
+                case "3":
+                    TransferMoney();
+
+                    break;
+                case "0":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+
+            BankAccount();
+        }
+
+        static void AddMoney()
+        {
+            Console.Write("Choose card number: ");
+            short from = short.Parse(Console.ReadLine());
+            from--;
+            Console.WriteLine($"money in this card {money[from]}$");
+
+            Console.WriteLine("How much u wanna add?");
+
+            double addingMoney = double.Parse(Console.ReadLine());
+
+            money[from] = money[from] + addingMoney;
+            Console.WriteLine($"now in this card {money[from]}$");
+        }
+        static void SubtractMoney()
+        {
+            Console.Write("Choose card number: ");
+
+            short from = short.Parse(Console.ReadLine());
+            from--;
+
+            Console.WriteLine($"money in this card {money[from]}$");
+
+            Console.WriteLine("How much u wanna subtract?");
+
+            double subtractingMoney = double.Parse(Console.ReadLine());
+
+            money[from] = money[from] - subtractingMoney;
+            Console.WriteLine($"now in this card {money[from]}$");
+        }
+        static void TransferMoney()
+        {
+            Console.Write("Choose card number: ");
+            short from = short.Parse(Console.ReadLine());
+            from--;
+
+            Console.WriteLine("Choose card number to transfer");
+            short to = short.Parse(Console.ReadLine());
+            to--;
+
+            Console.WriteLine("How much?");
+            double needTarnsfer = double.Parse(Console.ReadLine());
+
+            money[from] = money[from] - needTarnsfer;
+            money[to] = money[to] + needTarnsfer;
+        }
+
     }
 }
--- Programming/Bank Account System/obj/Debug/net8.0/Bank Account System.AssemblyInfo.cs
+++ Programming/Bank Account System/obj/Debug/net8.0/Bank Account System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank Account System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e2b8ac1bc637ef3d53e15d464e96b7b6d958bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f06ba0012506dbb10babe68b1dfafc15e3ac92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank Account System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank Account System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
