207eb37119cdef0a6a45e46665f897fd0892925d
Abdulloh Halilov <109743592+UZBeKHalilov@users.noreply.github.com>
2024-11-19 17:27:18 +0500

    async

---
+++ Programming/Background Calculation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Background_Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => { Calcualte(); });
+            Console.WriteLine("Background started!");
+        }
+
+        static void Calcualte()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
