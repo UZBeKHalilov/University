aca67dba482a3e9e35767b914786f247d4c4538c
Abdulloh Halilov <109743592+UZBeKHalilov@users.noreply.github.com>
2024-11-22 12:10:13 +0500

    async await

---
+++ Programming/async await/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using System.Net.Http;
+
+using System.Threading.Tasks;
+
+namespace async_await
+{
+    internal class Program
+    {
+        #region Learning
+        //static async Task Main(string[] args)
+        //{
+        //    string url = "https://jsonplaceholder.typicode.com/posts/1";
+
+        //    string result = await FetchDataAsync(url);
+
+        //    Console.WriteLine(result);
+
+        //    await Task.Delay(2000);
+
+        //    Console.WriteLine("Task completed without deadlock");
+
+        //}
+
+        //static async Task<string> FetchDataAsync(string url)
+        //{
+        //    using (HttpClient client = new HttpClient())
+        //    {
+        //        string response = await client.GetStringAsync(url);
+        //        return response;
+        //    }
+        //}
+
+        #endregion
+
+        #region Task
+
+        static async Task Main()
+
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts/1";
+
+            await FetchAndPrintDataAsync(url);
+
+        }
+
+        static async Task FetchAndPrintDataAsync(string url)
+
+        {
+
+            using (HttpClient client = new HttpClient())
+
+            {
+                string response = await client.GetStringAsync(url);
+
+                Console.WriteLine(response.Substring(0, 50));
+            }
+        }
+
+        #endregion
+    }
+}
