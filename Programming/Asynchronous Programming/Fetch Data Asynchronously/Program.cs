406ea97b6c2ef9631c546ee00cdd0828ab078fae
Abdulloh Halilov <109743592+UZBeKHalilov@users.noreply.github.com>
2024-11-23 10:20:05 +0500

    Asynchronous programming

---
+++ Programming/Asynchronous Programming/Fetch Data Asynchronously/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Fetch_Data_Asynchronously
+{
+    internal class Program
+    {
+        // ○ Create a task that simulates fetching data from an API by delaying for a few seconds and then printing “Data fetched.”
+        static void Main(string[] args)
+        {
+            Task FetchData = Task.Run(() =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("Fetching data from API...");
+                    Thread.Sleep(1000/i);
+                }
+
+                Console.WriteLine("Data fetched");
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main program doing some work...");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Main program End!");
+        }
+    }
+}
