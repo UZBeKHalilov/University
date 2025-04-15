d5c48f0731f64fa00ac897da29293f3cfe71c69a
Abdulloh Halilov <109743592+UZBeKHalilov@users.noreply.github.com>
2025-04-15 15:02:28 +0500

    Deleting not imported files

--- Programming/ECommerceAPI/Data/ECommerceDbContext.cs
+++ Programming/ECommerceAPI/Data/ECommerceDbContext.cs
@@ -12,7 +12,7 @@ namespace ECommerceAPI.Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<Category> Categories { get; set; }
+            public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
